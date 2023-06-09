3e4f94e63296b90998a5376a429f433b320d1169
ciposs <federicocpl@outlook.it>
2023-06-09 10:52:39 +0200

    Secondo Commit

--- app/src/main/java/com/example/mycinema/activities/MainActivity.kt
+++ app/src/main/java/com/example/mycinema/activities/MainActivity.kt
@@ -2,13 +2,11 @@ package com.example.mycinema.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mycinema.R
 import com.example.mycinema.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
